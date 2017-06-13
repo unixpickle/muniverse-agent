b38c45a96afd40be7f10603f3f4ce4da548483bb
Alex Nichol <unixpickle@gmail.com>
2017-06-13 13:21:21 -0400

    don't use NoHold for CartoonFlight
    Holding down keys makes sense in this game.

--- spec.go
+++ spec.go
@@ -25,12 +25,12 @@ var EnvSpecs = []*EnvSpec{
 	StandardKeySpec("Knightower-v0", true, 0.9, time.Second/8, 512),
 	StandardKeySpec("KumbaKarate-v0", true, 0.7, time.Second/10, 512),
 	StandardKeySpec("PenguinSkip-v0", true, 0.7, time.Second/5, 512),
-	StandardKeySpec("CartoonFlight-v0", true, 0.9, time.Second/8, 512),
-	StandardKeySpec("CartoonFlight-v1", true, 0.9, time.Second/8, 512),
 	StandardTapSpec("DontCrash-v0", true, 0.9, time.Second/10, 512),
 	StandardTapSpec("RabbitPunch-v0", true, 0.9, time.Second/8, 512),
 	StandardKeySpec("Twins-v0", false, 0.98, time.Second/10, 2048),
 	StandardKeySpec("RedHead-v0", false, 0.98, time.Second/10, 2048),
+	StandardKeySpec("CartoonFlight-v0", false, 0.9, time.Second/8, 512),
+	StandardKeySpec("CartoonFlight-v1", false, 0.9, time.Second/8, 512),
 }
 
 // SpecForName finds a specification in EnvSpecs.
